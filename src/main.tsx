7d973fca539c9c2c1f2d8992b184ad3a60a4bc9a
mrebolledo <maxi.rebolledo@gmail.com>
2026-01-19 11:34:05 -0300

    hash router gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import {createHashRouter, RouterProvider} from 'react-router'
 import Economy from "./pages/Economy.tsx";
 import Mid from "./pages/Mid.tsx";
 import Premium from "./pages/Premium.tsx";
 
 // Define tus rutas usando createBrowserRouter
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App />,
